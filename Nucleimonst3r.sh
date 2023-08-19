2572820e312592d19bf4bc664cba204780cd6b2e
Cyb3rspl0it <80126458+gl1tch0x1@users.noreply.github.com>
2023-08-19 20:35:28 +0545

    Update Nucleimonst3r.sh
    Here changes are made in lolcat, where user can install it automatically  without further code
     and,
    I've noticed a minor issue in the way the random quote is being selected from the array. The script is using the $RANDOM variable without a proper modulus operation, which could lead to errors.

--- Nucleimonst3r.sh
+++ Nucleimonst3r.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+
+# Check if lolcat is installed, and if not, install it
+if ! command -v lolcat &> /dev/null; then
+    echo "lolcat is not installed. Installing..."
+    
+    # Check package manager and install
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get install -y lolcat
+    elif command -v yum &> /dev/null; then
+        sudo yum install -y lolcat
+    elif command -v brew &> /dev/null; then
+        brew install lolcat
+    else
+        echo "Unsupported package manager. Please install lolcat manually."
+        exit 1
+    fi
+fi
 curl --silent "https://raw.githubusercontent.com/blackhatethicalhacking/Subdomain_Bruteforce_bheh/main/ascii.sh" | lolcat
 echo ""
-# Generate a random Sun Tzu quote for offensive security
+
 # Array of Sun Tzu quotes
 quotes=("The supreme art of war is to subdue the enemy without fighting." "All warfare is based on deception." "He who knows when he can fight and when he cannot, will be victorious." "The whole secret lies in confusing the enemy, so that he cannot fathom our real intent." "To win one hundred victories in one hundred battles is not the acme of skill. To subdue the enemy without fighting is the acme of skill.")
+
 # Get a random quote from the array
-random_quote=${quotes[$RANDOM % ${#quotes[@]}]}
+random_index=$((RANDOM % ${#quotes[@]}))
+random_quote="${quotes[random_index]}"
+
 # Print the quote
 echo "Offensive Security Tip: $random_quote - Sun Tzu" | lolcat
 sleep 1
